633ece6c0f1604e545253801a45db7a627cbd6f2
Tis Lais <tistististis@gmail.com>
2022-03-15 15:08:58 -0700

    Add delete hero functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -61,7 +61,21 @@ export class HeroService {
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap(_ => this.log(`update hero id=${hero.id}`)),
+    )
+  }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero w/ id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
     )
   }
 }
